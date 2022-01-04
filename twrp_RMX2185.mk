1ddb14e34e1bea129a147b1cd5a890188c71b9ad
HemanthJabalpuri <hemanthjabalpuri@gmail.com>
2022-01-04 09:50:28 +0530

    RMX2185: Use twrp-11 manifest * https://github.com/TeamWin/android_bootable_recovery/commit/2e344ab7fbd6d1af60b68c394db45c4de862e0f8 * https://github.com/TeamWin/android_bootable_recovery/commit/2bf400c47c6dd7ba1514145dd45ecc4c3d8ccedc

--- twrp_RMX2185.mk
+++ twrp_RMX2185.mk
@@ -20,10 +20,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := RMX2185
-PRODUCT_NAME := omni_RMX2185
+PRODUCT_NAME := twrp_RMX2185
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2185
 PRODUCT_MANUFACTURER := realme
